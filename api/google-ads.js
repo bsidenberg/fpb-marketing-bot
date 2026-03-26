e34ab3da0faebaf17ffac3900ed4ee3158bfd08e
bsidenberg <info@floridapolebarn.com>
2026-03-26 09:37:12 -0400

    Fix Google Ads API version to v15

--- api/google-ads.js
+++ api/google-ads.js
@@ -63,7 +63,7 @@ export default async function handler(req, res) {
       LIMIT 10
     `;
 
-    const apiUrl = `https://googleads.googleapis.com/v17/customers/${customerId}/googleAds:search`;
+    const apiUrl = `https://googleads.googleapis.com/v15/customers/${customerId}/googleAds:search`;
     console.log('=== GOOGLE ADS DEBUG ===');
     console.log('API URL:', apiUrl);
     console.log('Customer ID:', customerId);
